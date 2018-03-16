b56201458c249686cb5a25d76879f4f76425f673
scott88lee <scott88lee@gmail.com>
2018-03-16 18:13:15 +0800

    abstracted functions and vars, added more bots

--- js/script.js
+++ js/script.js
@@ -1,52 +1,101 @@
 //Declaring Variables
 var commPrice = 100;
-var difficulty = 1;		// 1-Easy 2-Medium 3-Hard
-var speed = 400; 		// Milliseconds
-var capital = 1000;
-var botAvail = 1000;
-var interval = 0; 		// Interval before direction change
-var direction = true; 	// Price up or Down
-var holdings = 0;
-var botHoldings = 0; 	 	// Initialise
-var assetValue = 0;
-var botAssetValue = 0;
-var profit = 0;
-var minPrice = 100;
-var maxPrice = 0;
-var ma10 = 0;
-var ma50 = 0;
-var rPNL = 0;
-var cost = [];
-var botCost = [];
-var trendCounter = 0;
-var comparator = [0,100];
-var sellCounter = 0;
-var agro = 2;
+var speed = 50; 			// Game speed in miliseconds
+var interval = 0; 			// Interval before direction change
+var direction = true; 		// Price up or Down
 
+var range = [100,100]; // Index 0 - High, 1 - Low
+
+// Player GLOBALS - index 0 - Player, 1 - Bot1, 2 - Bot2
+var player = [
+	{
+		capital : 1000,
+		holdings : 0,
+		holdingCost  : [],
+		totalCost : 0,
+		runningPNL : 0,
+		trendCounter : 0,
+		comparator : [0,100],
+		sellCounter : 0,
+		memory : [0,0],
+		agro : 2
+	},
+	{
+		capital : 1000,
+		holdings : 0,
+		holdingCost  : [],
+		totalCost : 0,
+		runningPNL : 0,
+		trendCounter : 0,
+		comparator : [0,100],
+		sellCounter : 0,
+		memory : [5,8],
+		agro : 2
+	},
+	{
+		capital : 1000,
+		holdings : 0,
+		holdingCost  : [],
+		totalCost : 0,
+		runningPNL : 0,
+		trendCounter : 0,
+		comparator : [0,100],
+		sellCounter : 0,
+		memory : [4,2],
+		agro : 2
+	},
+	{
+		capital : 1000,
+		holdings : 0,
+		holdingCost  : [],
+		totalCost : 0,
+		runningPNL : 0,
+		trendCounter : 0,
+		comparator : [0,100],
+		sellCounter : 0,
+		memory : [0,0],
+		agro : 2
+	}
+];
 
 //Shorten DOM calls
-var displayCapital = document.getElementById('capital');
-var displayBotAvail = document.getElementById('botAvail');
 var displayPrice = document.getElementById('price');
-var displayAsset = document.getElementById('asset');
-var displayBotAsset = document.getElementById('botAsset');
-var displayHoldings = document.getElementById('holdings');
-var displayBotHoldings = document.getElementById('botHoldings');
+
+var displayCapital = [	document.getElementById('capital'),
+						document.getElementById('capital1'),
+						document.getElementById('capital2'),
+						document.getElementById('capital3')];
+
+var displayCost = [	document.getElementById('cost'),
+					document.getElementById('cost1'),
+					document.getElementById('cost2'),
+					document.getElementById('cost3')];
+
+var displayHoldings = [	document.getElementById('holdings'),
+						document.getElementById('holdings1'),
+						document.getElementById('holdings2'),
+						document.getElementById('holdings3')];
+
+var displayRPNL = [	document.getElementById('rPNL'),
+					document.getElementById('rPNL1'),
+					document.getElementById('rPNL2'),
+					document.getElementById('rPNL3')];
+
 var displayHigh = document.getElementById('maxPrice');
 var displayLows = document.getElementById('minPrice');
 var display10ma = document.getElementById('ma10');
 var display50ma = document.getElementById('ma50');
-var displayrPNL = document.getElementById('rPNL');
-var displayBotRPNL = document.getElementById('botRPNL');
+
 var btnBuy = document.getElementById('btnBuy');
 var btnSell = document.getElementById('btnSell');
 
+/////////////////////////////////////////////////////////////
+btnBuy.addEventListener('click', function(){buy(0)});	  ///
+btnSell.addEventListener('click', function(){sell(0)});	  /// Add Listeners
+/////////////////////////////////////////////////////////////
 
-// Add Listeners
-btnBuy.addEventListener('click',buy);
-btnSell.addEventListener('click',function(){sell()});
 
-// Chart
+//\\//\\//\\//\\//\\//\\ Chart //\\//\\//\\//\\//\\//\\
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -73,11 +122,7 @@ var chart = new Chart(ctx, {
          }
     }
 });
-// Chart
 
-function difficulty(){
-	///
-}
 function addData(chart, label, data) {
     chart.data.labels.push(label);
     chart.data.datasets.forEach((dataset) => {
@@ -85,6 +130,8 @@ function addData(chart, label, data) {
     });
     chart.update();
 }
+//\\//\\//\\//\\//\\//\\ Chart //\\//\\//\\//\\//\\//\\
+
 function randomDuration(){
 	var duration = Math.floor(Math.random() * Math.floor(18));
 	return duration;	 //Generates random interval
@@ -98,168 +145,167 @@ function priceJump() { //Generates the price move amount
 	console.log(value);
 	return value;
 }
-function buy(){
-	console.log("BUY!!!!!!!!!!");
-	holdings++;
-	var tempSum=0;
-	capital -= commPrice;
-	cost.push(commPrice);
-	for (var i = 0; i<cost.length; i++){
-		tempSum += cost[i];
+function calMA(ticks){
+	var sumTemp = 0;
+	if (chart.data.datasets[0].data.length > ticks) {
+		for (var i = chart.data.datasets[0].data.length-ticks; i < chart.data.datasets[0].data.length; i++){
+			sumTemp += chart.data.datasets[0].data[i];
+		}
+	var avg = sumTemp / ticks;
+	avg = Math.round(avg * 100)/100;
 	}
-	displayAsset.innerText = tempSum;
-	capital = Math.round(capital * 100)/100;
-	//BUY!
+	if (avg > 0){
+	return avg; 
+	} else { return 100;} // Moving Averages Calculation
 }
-function botBuy(){
-	console.log("BotBUY!!!!!");
+function recordRange() {
+	if (commPrice > range[0]) {			// Calculate range
+		range[0] = commPrice;
+	}
+	if (commPrice < range[1]) {
+		range[1] = commPrice;
+	}
 }
-function sell(){
-	console.log("SELLL!!!!!!!!");
-	if (holdings > 0) {
-		holdings--;
-		capital += commPrice;
-		cost.shift();
-		capital = Math.round(capital * 100)/100;
+function updateDisplay(){
+	displayPrice.innerText = commPrice;
+
+	for (var i=0; i<player.length; i++){
+		displayCapital[i].innerText = player[i].capital;
+		displayHoldings[i].innerText = player[i].holdings;
+		displayCost[i].innerText = player[i].totalCost;
+		displayRPNL[i].innerText = player[i].runningPNL;
+	}
+}
+
+function buy(id) {
+	player[id].holdings++;  // Add to holdings
+	player[id].capital -= commPrice;  // Deduct from capital
+
+	player[id].holdingCost.push(commPrice);
+	
+	var tempBuySum = 0;
+	for (var i=0; i<player[id].holdingCost.length; i++){
+		tempBuySum += player[id].holdingCost[i];
+	}
+	player[id].totalCost = tempBuySum;
+
+	player[id].totalCost = Math.round(player[id].totalCost * 100)/100; //rounding
+	player[id].capital = Math.round(player[id].capital * 100)/100; //rounding
+
+	console.log(id + " BUY!!");
+	updateDisplay();
+}
+function sell(id){
+	if (player[id].holdings > 0) {
+		player[id].holdings--;
+		player[id].capital += commPrice;
+		player[id].holdingCost.shift();
+
+		var tempSum = 0;
+		for (var i=0; i<player[id].holdingCost.length; i++){
+		tempSum += player[id].holdingCost[i];
+		}
+
+		player[id].totalCost = tempSum;
+
+		player[id].totalCost = Math.round(player[id].totalCost * 100)/100; //rounding
+		player[id].capital = Math.round(player[id].capital * 100)/100;
 	//SELL!
-				if (holdings ===0){displayAsset.innerText = 0;}
+		if (player[id].holdings ===0)       //Final sale resets display to zero
+			{displayCost[id].innerText = 0;}
 	} else {
 		alert("You can't do naked shorts, buy the asset first.");
 	}
 }
-function botSell(){
-	console.log("BotSell!!!!!");
-}
-function cal10ma(){
-	var sum10 = 0;
-	if (chart.data.datasets[0].data.length > 10) {
-		for (var i = chart.data.datasets[0].data.length-10; i < chart.data.datasets[0].data.length; i++){
-			sum10 += chart.data.datasets[0].data[i];
-		}
-	var avg10 = sum10 / 10;
-	ma10 = Math.round(avg10 * 100)/100;
-	}
-}
-function cal50ma(){
-	var sum50 = 0;
-	if (chart.data.datasets[0].data.length > 50) {
-		for (var i = chart.data.datasets[0].data.length-50; i < chart.data.datasets[0].data.length; i++){
-			sum50 += chart.data.datasets[0].data[i];
-		}
-	var avg50 = sum50 / 50;
-	ma50 = Math.round(avg50 * 100)/100;
-	}
-}
-function runningPNL(){
-	var tempSum = 0;
-	if (holdings > 0){
-	for (var i = 0; i<cost.length; i++){
-		tempSum += cost[i];
-	}
-	rPNL = (commPrice * holdings) - tempSum;
-	}
-	if (holdings ===0){
-	rPNL = 0;}
-	rPNL = Math.round(rPNL * 100)/100;
-	displayrPNL.innerText = rPNL;
-}
-function botThink1(arg){
-	//set currentPrice to comparator
-	comparator.push(commPrice); // [0,100,99]
 
-	if (comparator[1] > comparator[2]) {
-		trendCounter++;
-		console.log("trendCounter++")
+
+//==============================================================\\		___
+function bot(arg,id){					//		   BOT CODE  	//	   (0 0)  
+	//set currentPrice to comparator							//   __| - |__    
+	player[id].comparator.push(commPrice); // [0,100,99]		//  //| o=o |\\  
+																// <] | o=o | [>	  
+	if (player[id].comparator[1] > player[id].comparator[2]) {	//	  \=====/
+		player[id].trendCounter++;								//     *****  						//    
 	}
 
-	if (trendCounter >= 8 && comparator[2]>comparator[1]) {
+	if (player[id].trendCounter >= player[id].memory[1] && player[id].comparator[2]>player[id].comparator[1]) {
 		for (var i = 0; i < arg; i++){
-			buy();
+			buy(id);
 		}
-		sellCounter +=5;
-		trendCounter = 0;
+		player[id].sellCounter += player[id].memory[0];
+		player[id].trendCounter = 0;
 	}
 
-	if (comparator[2] > comparator[1]) {
-		trendCounter = 0; 
+	if (player[id].comparator[2] > player[id].comparator[1]) {
+		player[id].trendCounter = 0; 
 	}
 
-	comparator.shift();
+	player[id].comparator.shift();
 
-	if (sellCounter > 1) {
-		sellCounter--;
-	} else if ( sellCounter === 1) {
+	if (player[id].sellCounter > 1) {
+		player[id].sellCounter--;
+	} else if ( player[id].sellCounter === 1) {
 		for (var i = 0; i < arg; i++){
-			sell();
-			calAgro();
+			sell(id);
+			calAgro(id);
 		}
-		sellCounter = 0;
+		player[id].sellCounter = 0;
 	}
 }
-function calAgro(){
-	if (capital > 1000) {
-		agro = Math.round(capital/300);
-	}
-}
+function calAgro(id){
+	if (player[id].capital > 1000) {							//
+		player[id].agro = Math.round(player[id].capital/200);	//	
+	}															//	
+}																//
+//==============================================================\\
+
+//////////////////////////////////////////////////////////
+function game() {
+
+	updateDisplay();
+
+	displayHigh.innerText = range[0];
+	displayLows.innerText = range[1];
+	display10ma.innerText = calMA(10);
+	display50ma.innerText = calMA(50);
+	//runningPNL(); //display rPNL
+	//botRunningPNL();//display Bot RPNL
+
+	// Calculate indicators
+	recordRange();
+
+	bot(player[1].agro,1);
+	bot(player[2].agro,2);
 
 
-function updateDisplay() {
-	displayHoldings.innerText = holdings;
-	displayBotHoldings.innerText = botHoldings;
-	displayPrice.innerText = commPrice;
-	displayCapital.innerText = capital;
-	displayBotAvail.innerText = botAvail;
-	displayHigh.innerText = maxPrice;
-	displayLows.innerText = minPrice;
-	display10ma.innerText = ma10;
-	display50ma.innerText = ma50;
-	runningPNL(); //display rPNL
-
-	//difficulty();
-	cal50ma();
-	cal10ma();
-	botThink1(agro);
-
-
+///////////////////////////////////////////////////////////
+///////////////// CHART MECHANICS /////////////////////////
 	addData(chart, " ", commPrice)
-
 	interval--;
-	if (interval <= 0){
+	if (interval <= 0){					//Chart direction
 		interval = randomDuration();
 		direction = !direction;
 	}
-	if (commPrice <= 50){  // Price will never go to ZERO
+	if (commPrice <= 50){       		// Price will never go to ZERO
 		direction = true;
 	}
-
-	if ( direction === true){
+	if ( direction === true){			// Changes the commPrice
 		commPrice += priceJump();
 		commPrice = Math.round(commPrice * 100)/100;
 	} else {
 		commPrice -= priceJump();
 		commPrice = Math.round(commPrice * 100)/100;		
 	}
-	if (chart.data.datasets[0].data.length > 88){
+	if (chart.data.datasets[0].data.length > 88){   // Total num of entries on chart
     	chart.data.datasets[0].data.shift();
     	chart.data.labels.shift();
     }
-
-	if (capital <= 0){
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+	if (player[0].capital <= 0){
 		alert("You're broke! GAME OVER!");			//STOPS THE GAME WHEN NO MONEY
-		clearInterval(myVar);
-	}
-
-	if (assetValue < 0){
-		netAssetValueDisplay = 0;
-	} else {
-		netAssetValueDisplay = assetValue;
-	} // Main Game Loop
-
-	if (commPrice > maxPrice) {
-		maxPrice = commPrice;
-	}
-	if (commPrice < minPrice) {
-		minPrice = commPrice;
+		clearInterval(FMX);
 	}
 }
-var myVar = setInterval(updateDisplay,speed); //Init Loop
+
+var FMX = setInterval(game,speed); //Init Loop
